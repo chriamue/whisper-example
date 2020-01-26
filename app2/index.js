77f9c9cdca7d647c74012932d431e6e72c4404e9
Christian M <chriamue@gmail.com>
2020-01-26 15:11:50 +0100

    updated apps connecting whisper

--- app2/index.js
+++ app2/index.js
@@ -1,1 +1,20 @@
-console.log('app 2');
+const Shh = require('web3-shh');
+const shh = new Shh('ws://node2:8546');
+
+shh.getInfo()
+    .then(console.log);
+
+shh.net.getId()
+    .then((id) => { console.log('Network ID:', id) });
+
+shh.net.getPeerCount()
+    .then((count) => { console.log('Peers:', count) });
+
+shh.newKeyPair()
+    .then((id) => {
+        console.log('New Keypair id:', id)
+        shh.getPrivateKey(id)
+            .then(console.log);
+        shh.getPublicKey(id)
+            .then(console.log);
+    });
